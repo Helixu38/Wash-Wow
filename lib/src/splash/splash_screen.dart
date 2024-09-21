88df0f7a4a415cddebe8a0f216ead16fb2bd21c6
helixu38 <you@example.com>
2024-09-21 15:15:55 +0700

    feat: added splash screen with logo

---
+++ lib/src/splash/splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/logos/Logomarkwhite.png')),
+        color: const Color.fromRGBO(4, 90, 208, 1),
+      ),
+    ));
+  }
+}
