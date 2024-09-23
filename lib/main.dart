bac1e1f44435bc8bdbd8be9da73ce891d8009944
helixu38 <you@example.com>
2024-09-23 22:19:15 +0700

    chore(env): added env for api key

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:wash_wow/src/splash/splash_screen.dart';
+  import 'package:flutter/material.dart';
+  import 'package:wash_wow/src/splash/splash_screen.dart';
+  import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load(fileName: ".env.local");
+    runApp(MyApp());
   }
-}
+
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return const MaterialApp( 
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+        
+      );
+    }
+  }
 
 
