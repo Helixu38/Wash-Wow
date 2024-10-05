0d83c88bc45346f3f3cb694e17e31560e24196ec
helixu38 <vipase2004@gmail.com>
2024-10-05 23:47:47 +0700

    feat: implement get current location from phone

--- lib/src/service/services_screen.dart
+++ lib/src/service/services_screen.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
-class ServicesScreen extends StatelessWidget {
+class ServicesScreen extends StatefulWidget {
   final String serviceName;
   const ServicesScreen({super.key, required this.serviceName});
+  static const RoundedRectangleBorder roundedRectangleBorder =
+      RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12.0)));
+
+  @override
+  State<ServicesScreen> createState() => _ServicesScreenState();
+}
+
+class _ServicesScreenState extends State<ServicesScreen> {
+  String? _locationText = "Fetching location...";
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition(); // Fetch location on init
+  }
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _locationText = "Location services are disabled.";
+      });
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _locationText = "Location permissions are denied";
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _locationText =
+            "Location permissions are permanently denied, we cannot request permissions.";
+      });
+      return;
+    }
+
+    // When we reach here, permissions are granted and we can get the location.
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+
+    setState(() {
+      _locationText =
+          "${position.latitude.toStringAsFixed(4)}, ${position.longitude.toStringAsFixed(4)}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Center(
-        child: Text(
-          serviceName,
-        ),
-      ),
+      body: buildBody(),
     );
   }
 
@@ -22,11 +80,130 @@ class ServicesScreen extends StatelessWidget {
         preferredSize: const Size.fromHeight(80.0),
         child: AppBar(
           title: Text(
-            serviceName,
+            widget.serviceName,
             style: TextStyle(
               fontWeight: FontWeight.w900,
+              color: Colors.white,
             ),
-            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).primaryColor,
+          shape: ServicesScreen.roundedRectangleBorder,
+          iconTheme: IconThemeData(color: Colors.white),
         ));
+  }
+
+  Widget buildBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          child: buildLocation("Địa chỉ gần bạn", MdiIcons.map),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLocation(String title, IconData icon) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start, // Align to start
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  icon,
+                  color: Theme.of(context).primaryColor,
+                  size: 24,
+                ),
+                const SizedBox(width: 8), // Optional spacing
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 20,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    print('Xem thêm clicked');
+                  },
+                  child: Row(
+                    children: [
+                      Text(
+                        "xem thêm",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 14,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      Transform.rotate(
+                        angle: 270 * math.pi / 180, // Rotating icon
+                        child: const Icon(
+                          Icons.expand_more,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 8), // Optional spacing between elements
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            crossAxisAlignment:
+                CrossAxisAlignment.start, // Align items to start
+            children: [
+              Text(
+                "Địa điểm",
+                style: TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 8), // Spacing below "Địa điểm"
+              Row(
+                children: [
+                  Icon(
+                    MdiIcons.mapMarker, // Marker icon
+                    size: 16,
+                    color: Colors.black54,
+                  ),
+                  const SizedBox(width: 4), // Space between icon and text
+                  Text(
+                    _locationText ?? "Unknown location", // Display fetched location
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  const SizedBox(width: 4), // Space between text and icon
+                  Icon(
+                    Icons.expand_more, // Expand-down icon
+                    size: 16,
+                    color: Colors.black54,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_app_check
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
+import geolocator_apple
 import path_provider_foundation
 import sqflite
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
